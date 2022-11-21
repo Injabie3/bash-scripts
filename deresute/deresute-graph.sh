78d85464065e2318409f72b17f6f5eff99084002
Injabie3 <injabie3@gmail.com>
2022-11-20 21:43:23 -0800

    deresute: Dynamically figure out script location
    - Reference https://stackoverflow.com/questions/59895

--- deresute/deresute-graph.sh
+++ deresute/deresute-graph.sh
@@ -7,7 +7,19 @@
 # deresute.me Image Archiver
 # Archive info from game.
 # 
-scriptPath="$HOME/scripts/deresute"
+
+# Get script directory, reference https://stackoverflow.com/questions/59895
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path
+  # where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+scriptPath="$DIR"
 
 # Grab user key/value pairs: key ID, private ID value
 source $scriptPath/deresute-config.sh
